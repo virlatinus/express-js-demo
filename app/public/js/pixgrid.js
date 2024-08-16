ea77c2c2915c62359d875d9fb1d1c34e34c74bcc
Werner Garcia <werner.garcia@gmail.com>
2024-08-16 12:41:37 -0600

    Add jQuery to pixgrid code

--- app/public/js/pixgrid.js
+++ app/public/js/pixgrid.js
@@ -1,82 +1,73 @@
-$(document).ready(() => {
-
-  function centerImage(theImage) {
-    var myDifX = (window.innerWidth - theImage.width) / 2,
-      myDifY = (window.innerHeight - theImage.height) / 2;
-    return (
-      (theImage.style.top = myDifY + "px"),
-      (theImage.style.left = myDifX + "px"),
-      theImage
-    );
+$(document).on("click", ".pixgrid", (e) => {
+  let centerImage = (theImage) => {
+    let myDifX = (window.document.body.clientWidth - theImage.prop("width")) / 2,
+      myDifY = (window.innerHeight - theImage.prop("height")) / 2;
+    theImage.css({ top: myDifY + "px", left: myDifX + "px" });
   };
 
-  document.querySelector(".pixgrid").addEventListener("click", function(e) {
-      if ("IMG" === e.target.tagName) {
-        var myOverlay = document.createElement("div");
+  if ("IMG" === e.target.tagName) {
+    let myOverlay = $("<div>", {
+      id: "overlay",
+      css: {
+        overflow: 'hidden',
+        position: "absolute",
+        top: 0,
+        backgroundColor: "rgba(0, 0, 0, 0.7)",
+        cursor: "pointer",
+        width: window.document.body.clientWidth + "px",
+        height: window.innerHeight + "px",
+        top: window.scrollY + "px",
+        left: window.scrollX + "px",
+      },
+    });
+    $("body").append(myOverlay);
 
-        (myOverlay.id = "overlay"),
-          document.body.appendChild(myOverlay),
-          (myOverlay.style.position = "absolute"),
-          (myOverlay.style.top = 0),
-          (myOverlay.style.backgroundColor = "rgba(0,0,0,0.7)"),
-          (myOverlay.style.cursor = "pointer"),
-          (myOverlay.style.width = window.innerWidth + "px"),
-          (myOverlay.style.height = window.innerHeight + "px"),
-          (myOverlay.style.top = window.pageYOffset + "px"),
-          (myOverlay.style.left = window.pageXOffset + "px");
+    let imageSrc = e.target.src;
+    let largeImage = $("<img>", {
+      id: "largeImage",
+      src: imageSrc.substr(0, imageSrc.length - 7) + ".jpg",
+      css: { display: "block", position: "absolute" },
+    });
 
-        var imageSrc = e.target.src,
-          largeImage = document.createElement("img");
+    largeImage.on("load", (t) => {
+      let that = $(t.target);
 
-        (largeImage.id = "largeImage"),
-          (largeImage.src = imageSrc.substr(0, imageSrc.length - 7) + ".jpg"),
-          (largeImage.style.display = "block"),
-          (largeImage.style.position = "absolute"),
-          largeImage.addEventListener("load", function() {
-            this.height > window.innerHeight &&
-              ((this.ratio = window.innerHeight / this.height),
-              (this.height = this.height * this.ratio),
-              (this.width = this.width * this.ratio)),
-              this.width > window.innerWidth &&
-                ((this.ratio = window.innerWidth / this.width),
-                (this.height = this.height * this.ratio),
-                (this.width = this.width * this.ratio)),
-              centerImage(this),
-              myOverlay.appendChild(largeImage);
-          }),
-          largeImage.addEventListener(
-            "click",
-            function() {
-              myOverlay &&
-                (window.removeEventListener("resize", window, !1),
-                window.removeEventListener("scroll", window, !1),
-                myOverlay.parentNode.removeChild(myOverlay));
-            },
-            !1
-          ),
-          window.addEventListener(
-            "scroll",
-            function() {
-              myOverlay &&
-                ((myOverlay.style.top = window.pageYOffset + "px"),
-                (myOverlay.style.left = window.pageXOffset + "px"));
-            },
-            !1
-          ),
-          window.addEventListener(
-            "resize",
-            function() {
-              myOverlay &&
-                ((myOverlay.style.width = window.innerWidth + "px"),
-                (myOverlay.style.height = window.innerHeight + "px"),
-                (myOverlay.style.top = window.pageYOffset + "px"),
-                (myOverlay.style.left = window.pageXOffset + "px"),
-                centerImage(largeImage));
-            },
-            !1
-          );
+      if (that.prop("height") > window.innerHeight) {
+        let ratio = window.innerHeight / that.prop("height");
+        that.prop("height", that.prop("height") * ratio);
+        that.prop("width", that.prop("width") * ratio);
       }
-    },
-    !1
-  );
+
+      if (that.prop("width") > window.document.body.clientWidth) {
+        let ratio = window.document.body.clientWidth / that.prop("width");
+        that.prop("height", that.prop("height") * ratio);
+        that.prop("width", that.prop("width") * ratio);
+      }
+      centerImage(that);
+      myOverlay.append(largeImage);
+    });
+
+    largeImage.on("click", () => {
+      myOverlay && ($(window).off("resize scroll"), myOverlay.remove());
+    });
+
+    $(window).on("scroll", () => {
+      myOverlay &&
+        myOverlay.css({
+          top: window.scrollY + "px",
+          left: window.scrollX + "px",
+        });
+    });
+
+    $(window).on("resize", () => {
+      myOverlay &&
+        (myOverlay.css({
+          width: window.document.body.clientWidth + "px",
+          height: window.innerHeight + "px",
+          top: window.scrollY + "px",
+          left: window.scrollX + "px",
+        }),
+        centerImage(largeImage));
+    });
+  }
 });
