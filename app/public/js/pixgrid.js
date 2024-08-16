e7a034f15f2512467a1206345e597576cb0291fa
Werner Garcia <werner.garcia@gmail.com>
2024-08-16 12:47:08 -0600

    Refactored ifs

--- app/public/js/pixgrid.js
+++ app/public/js/pixgrid.js
@@ -1,8 +1,8 @@
 $(document).on("click", ".pixgrid", (e) => {
-  let centerImage = (theImage) => {
-    let myDifX = (window.document.body.clientWidth - theImage.prop("width")) / 2,
-      myDifY = (window.innerHeight - theImage.prop("height")) / 2;
-    theImage.css({ top: myDifY + "px", left: myDifX + "px" });
+  let centerImage = (img) => {
+    let myDifX = (window.document.body.clientWidth - img.prop("width")) / 2,
+      myDifY = (window.innerHeight - img.prop("height")) / 2;
+    img.css({ top: myDifY + "px", left: myDifX + "px" });
   };
 
   if ("IMG" === e.target.tagName) {
@@ -29,45 +29,50 @@ $(document).on("click", ".pixgrid", (e) => {
       css: { display: "block", position: "absolute" },
     });
 
-    largeImage.on("load", (t) => {
-      let that = $(t.target);
+    largeImage.on("load", (e) => {
+      let img = $(e.target);
 
-      if (that.prop("height") > window.innerHeight) {
-        let ratio = window.innerHeight / that.prop("height");
-        that.prop("height", that.prop("height") * ratio);
-        that.prop("width", that.prop("width") * ratio);
+      if (img.prop("height") > window.innerHeight) {
+        let ratio = window.innerHeight / img.prop("height");
+        img.prop("height", img.prop("height") * ratio);
+        img.prop("width", img.prop("width") * ratio);
       }
 
-      if (that.prop("width") > window.document.body.clientWidth) {
-        let ratio = window.document.body.clientWidth / that.prop("width");
-        that.prop("height", that.prop("height") * ratio);
-        that.prop("width", that.prop("width") * ratio);
+      if (img.prop("width") > window.document.body.clientWidth) {
+        let ratio = window.document.body.clientWidth / img.prop("width");
+        img.prop("height", img.prop("height") * ratio);
+        img.prop("width", img.prop("width") * ratio);
       }
-      centerImage(that);
+      centerImage(img);
       myOverlay.append(largeImage);
     });
 
     largeImage.on("click", () => {
-      myOverlay && ($(window).off("resize scroll"), myOverlay.remove());
+      if (myOverlay) {
+        $(window).off("resize scroll");
+        myOverlay.remove();
+      }
     });
 
     $(window).on("scroll", () => {
-      myOverlay &&
+      if (myOverlay) {
         myOverlay.css({
           top: window.scrollY + "px",
           left: window.scrollX + "px",
         });
+      }
     });
 
     $(window).on("resize", () => {
-      myOverlay &&
-        (myOverlay.css({
+      if (myOverlay) {
+        myOverlay.css({
           width: window.document.body.clientWidth + "px",
           height: window.innerHeight + "px",
           top: window.scrollY + "px",
           left: window.scrollX + "px",
-        }),
-        centerImage(largeImage));
+        });
+        centerImage(largeImage);
+      }
     });
   }
 });
